fb41be4442a2996555d0406ecfb20a0fa99f982e
Dương Nguyễn Phú Cường <kellyfire611@gmail.com>
2024-07-11 12:32:42 +0700

    Fix test case

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
       <h1>Xin chào Dương Nguyễn Phú Cường | DevSecOps</h1>
       <p>Đây là đoạn nội dung chức năng 1</p>
 
-      <h1 className="text-3xl font-bold underline text-blue-500">Hello world!</h1>
+      <p className="text-3xl font-bold underline text-blue-500">Hello world!</p>
     </>
   );
 }
